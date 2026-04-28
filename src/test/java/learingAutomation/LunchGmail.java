1beb5c51af5d9dfeca28170a8dbacbf96c6564cf
Sanjeeb Kumar <sanjeeb.pattnaik3@gmail.com>
2026-04-29 02:33:01 +0530

    trying manually fail

--- src/test/java/learingAutomation/LunchGmail.java
+++ src/test/java/learingAutomation/LunchGmail.java
@@ -19,18 +19,18 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LunchGmail {
- //private String actualTitle;
+ private String actualTitle;
     @Test
     public void openGoogle() {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com");
         System.out.println(driver.getTitle());
         
-//		if (!actualTitle.contains("Toogle")) {
-//            Assert.fail("Title does not match");
-//        }
-        driver.quit();
-        System.out.println("successfully completed");
+		if (!actualTitle.contains("Toogle")) {
+            Assert.fail("Title does not match");
+        }
+//        driver.quit();
+//        System.out.println("successfully completed");
     }
 }
 
